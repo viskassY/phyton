d515b9c58cef1e5f226cf64eff05d481832fd12e
viskassY <arutaviktoria5@gmail.com>
2026-02-16 23:02:59 +0300

    added func

---
+++ interpreter.py
@@ -0,0 +1,14 @@
+def explore_text():
+    word = "Python"
+    print(f"Слово: {word}\n")
+
+    codes = [ord(char) for char in word]
+    print(f"Числове представлення: {codes}")
+
+    binary = [bin(code)[2:].zfill(8) for code in codes]
+    print(f"Двійковий код: {binary}")
+
+    decoded = "".join([chr(code) for code in codes])
+    print(f"\n Зворотне перетворення: {decoded}")
+
+explore_text()
