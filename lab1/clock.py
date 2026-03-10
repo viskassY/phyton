0726e43728789e66e41e389e6a8312b08e8be49b
viskassY <arutaviktoria5@gmail.com>
2026-03-10 13:01:41 +0300

    Redirected files to the lab1

--- lab1/clock.py
+++ lab1/clock.py
@@ -3,7 +3,6 @@ def get_full_clock_angle(h, m, s):
     sec_angle = s * 6
     min_angle = (m * 6) + (s * 0.1)
     
-    # 0.00833 — 0.5 град / 60 сек
     hour_angle = ((h % 12) * 30) + (m * 0.5) + (s * 0.00833)
     angle = abs(hour_angle - min_angle)
     
@@ -11,4 +10,4 @@ def get_full_clock_angle(h, m, s):
         angle = 360 - angle
         
     return round(angle, 2) 
-print(f"Кут: {get_full_clock_angle(15, 30, 10)}°")
+print(f"Кут: {get_full_clock_angle(8, 40, 30)}°")
